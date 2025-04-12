1e8e8078e1b8ba031081621b3ade7cabbb3a3843
Christina Dobbins <u800176908@byui.edu>
2025-04-12 08:21:52 -0600

    refactor: update comments for clarity and disable unused code in server.js

--- scripts/server.js
+++ scripts/server.js
@@ -1,4 +1,5 @@
-// Dynamically imports app.js using ES Modules syntax.
+//Are you absolutly sure you need to modify this file. Check again.
+// // Dynamically imports app.js using ES Modules syntax.
 // Starts the server by calling app.listen(port).
 // Emtry point.
 // filepath: c:\Livestock-Lineup\scripts\server.js
@@ -6,17 +7,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { fileURLToPath, pathToFileURL } from "url";
+// import { fileURLToPath, pathToFileURL } from "url";
 import path from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const appPath = path.join(__dirname, "./staticServer.js");
-console.log("Loading Static Server from:", appPath);
+// const appPath = path.join(__dirname, "./staticServer.js");
+// console.log("Loading Static Server from:", appPath);
 
-const appUrl = pathToFileURL(appPath).href;
-const { default: app } = await import(appUrl);
+// const appUrl = pathToFileURL(appPath).href;
+// const { default: app } = await import(appUrl);
 
 const port = process.env.PORT || 3000;
 
