c1a36f87327e091dd2f08fd5a65521a18cdddcda
Christina Dobbins <u800176908@byui.edu>
2025-04-11 21:57:09 -0600

    refactor: update app.js to import pool from db.js for improved modularity

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
-// Does this file need to go into the uiHandlers.bundle.js file? Also which html file does it belong in if any? If it doesn't belong in an HTML or any of the other files, how is it initialized, called or used? What is it's purpose? Please, verify, ensure that this file is updated to use ES Modals and dont use the weird notations. ensure that all variables, functions, and wording are consistent across files and that everything links properly.
-
 import express from "express";
 import cors from "cors";
 import path from "path";
 import Pusher from "pusher";
-import { Pool } from "pg";
+import pool from "./db.js";
 import { fileURLToPath } from "url";
 import routeEx from "../routes/route-ex.js";
 import routeOr from "../routes/route-or.js";
