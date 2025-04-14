e115c98019ab0d14335ace6c1b9bfd5007a6793e
Christina Dobbins <u800176908@byui.edu>
2025-04-14 11:33:42 -0600

    modifid db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import pkg from "pg";
-const { Pool } = pkg; 
+const { Pool } = pkg;
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
