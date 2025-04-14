9fc632669a65a843b30975765f7785c3d061aba0
Christina Dobbins <u800176908@byui.edu>
2025-04-13 20:42:06 -0600

    refactor: remove unnecessary comments and streamline imports in server.js

--- scripts/server.js
+++ scripts/server.js
@@ -1,23 +1,7 @@
-//Are you absolutly sure you need to modify this file. Check again.
-// // Dynamically imports app.js using ES Modules syntax.
-// Starts the server by calling app.listen(port).
-// Emtry point.
-// filepath: c:\Livestock-Lineup\scripts\server.js
-
 import dotenv from "dotenv";
 dotenv.config();
 
-// import { fileURLToPath, pathToFileURL } from "url";
-import app from "../src/app.js"
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const appPath = path.join(__dirname, "./staticServer.js");
-console.log("Loading Static Server from:", appPath);
-
-const appUrl = pathToFileURL(appPath).href;
-const { default: app } = await import(appUrl);
+import app from "../src/app.js";
 
 const port = process.env.PORT || 3000;
 
